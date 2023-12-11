3c67dd0667fb2e77175ad2afa7490dac1c354cd0
Syed Ashhad <syedashhad17@gmail.com>
2023-12-11 20:40:20 +0500

    MVC

---
+++ Aptech All Projects/Student Management/Student Management/Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Student_Management.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult StudentProfile()
+        {
+            return View();
+        }
+    }
+}
---
+++ Aptech All Projects/The Student Portal/The Student Portal/Controllers/StudentManagementController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace The_Student_Portal.Controllers
+{
+    public class StudentManagementController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
