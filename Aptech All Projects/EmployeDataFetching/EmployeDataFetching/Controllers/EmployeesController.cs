513205a513a0010d4add6e057d616b74181f158e
Syed Ashhad <syedashhad17@gmail.com>
2023-12-13 20:33:02 +0500

    MVC Data Fetching

---
+++ Aptech All Projects/EmployeDataFetching/EmployeDataFetching/Controllers/EmployeesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EmployeDataFetching.Models;
+namespace EmployeDataFetching.Controllers
+
+{
+    public class EmployeesController : Controller
+    {
+        public IActionResult employeeDataFetch()
+        {
+            EmployeeData Ashhad = new EmployeeData
+            {
+                id = 1,
+                name = "Syed Ashhad",
+                age = 23,
+                description = "This Is Syed Ashhad And This Data Is Fetching By MVC"
+            };
+            ViewBag.ashhadData = Ashhad;
+            return View();
+        }
+    }
+}
---
+++ Aptech All Projects/EmployeDataFetching/EmployeDataFetching/Models/EmployeeData.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeDataFetching.Models
+{
+    public class EmployeeData
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public string description { get; set; }
+       
+    }
+}
