cd60bc765ec403827cbe422b05e84c6464dd34a1
Syed Ashhad <syedashhad17@gmail.com>
2024-01-24 20:12:32 +0500

    ....

--- Aptech All Projects/Ecommers/Ecommers/Models/Book.cs
+++ Aptech All Projects/Ecommers/Ecommers/Models/Book.cs
@@ -2,6 +2,13 @@
 {
     public class Book
     {
+        public int Id { get; set; }
+        public string BookName { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+        public string Author { get; set; }
 
+        public Category category;
+        public int CategoryId { get; set; }
     }
 }
